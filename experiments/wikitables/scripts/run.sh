4e2e06db6385e953627f5605b0b5f9baae7f7edd
Shivanshu Gupta <shivanshugupta1995@gmail.com>
2018-01-15 20:38:08 +0530

    some config changes for experiments

--- experiments/wikitables/scripts/run.sh
+++ experiments/wikitables/scripts/run.sh
@@ -2,4 +2,5 @@
 
 CLASSPATH="target/scala-2.11/pnp-assembly-0.1.2.jar:lib/stanford-corenlp-3.6.0-models.jar"
 echo $CLASSPATH
-java -Djava.library.path=lib -classpath $CLASSPATH -Xmx12000M "$@"
+# Changed from 12 to 30 Gigs
+java -Djava.library.path=lib -classpath $CLASSPATH -Xmx30000M "$@" 
